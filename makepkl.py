ce025d52538a90a4c1087ed6374f600cd1984896
srijith9862 <59470043+srijith9862@users.noreply.github.com>
2021-12-05 11:36:51 +0530

    Update makepkl.py

--- makepkl.py
+++ makepkl.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Makes a .pkl file which can be for loading data into the template
 def main():
-    plantsFile = 'data/telugu.csv'
+    plantsFile = 'data/Telugu.csv'
     plantsDF = pd.read_csv(plantsFile)
     pickle.dump(plantsDF, open('./123.pkl', 'wb'))
 
